b2cbdfe53086c0b22695207ddbbbae68ed479db0
Garrett Moseke <gmoseke@protonmail.com>
2024-03-09 18:25:03 +0100

    chore: stub out basic backlog routes

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,49 @@ fn ping() -> &'static str {
     "pong"
 }
 
+// TODO: move these to their own file/handler crate
+#[get("/")]
+fn list_backlog_entries() {
+    unimplemented!()
+    // Return all
+    // TODO: allow pagination, sorting, type args
+}
+
+#[post("/item")]
+fn create_backlog_entry() {
+    unimplemented!()
+    /*
+    Request content needs:
+        - Item type enum
+        - Title
+        - additional metadata per type
+     */
+
+    // TODO: store by unique ID
+}
+
+#[get("/item?<id>")]
+fn get_backlog_entry(id: &str) {
+    unimplemented!()
+    /* 
+    Request content needs:
+        - unique ID of backlog entry
+    */
+}
+
+#[delete("/item?<id>")]
+fn remove_backlog_entry(id: &str) {
+    unimplemented!()
+    /* 
+    Request content needs:
+        - unique ID of backlog entry
+    */
+}
+
 #[launch]
 fn rocket() -> _ {
     // Mount paths by namespace
-
-    // Util routes
-    rocket::build().mount("/util", routes![ping])
+    rocket::build()
+        .mount("/util", routes![ping])
+        .mount("/backlog", routes![list_backlog_entries, create_backlog_entry, get_backlog_entry, remove_backlog_entry])
 }
