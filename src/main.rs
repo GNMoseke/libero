29743133d85c7d28f0317f11c4a7a1e83627e458
Garrett Moseke <gmoseke@protonmail.com>
2024-03-09 18:09:25 +0100

    feat(New Route): add /util/ping route as health check

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+#[get("/ping")]
+fn ping() -> &'static str {
+    "pong"
+}
+
+#[launch]
+fn rocket() -> _ {
+    // Mount paths by namespace
+
+    // Util routes
+    rocket::build().mount("/util", routes![ping])
 }
