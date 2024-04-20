a56f44f593ce7b1449c9d8332a137de3568e98d8
Garrett <gmoseke@protonmail.com>
2024-04-20 12:02:59 +0200

    feat: working APOD refresh & download

--- src/apod.rs
+++ src/apod.rs
@@ -1,6 +1,7 @@
-use image;
 use serde::Deserialize;
 use std::env;
+use std::ffi::OsStr;
+use std::path;
 
 #[derive(Debug, Deserialize)]
 struct APOD {
@@ -14,17 +15,18 @@ struct APOD {
     url: String
 }
 
-fn download_apod() {
+pub fn download_apod() {
     info!("Starting Astronomy Picture of the Day download...");
     // get json response, grab high quality image URL
-    let api_key = env::var("API_KEY").unwrap_or("DEMO_KEY".to_owned());
+    let file_store_dir = env::var("APOD_PATH").unwrap_or("/tmp/".to_owned());
+    let api_key = env::var("APOD_API_KEY").unwrap_or("DEMO_KEY".to_owned());
     let endpoint = "https://api.nasa.gov/planetary/apod?api_key=".to_owned() + &api_key;
     let api_resp = attohttpc::get(endpoint)
-        .send()
+    .send()
         .expect("Could not retrieve the APOD");
     let raw_text = api_resp
         .text()
-        .expect("Could not convert the respoinse to text")
+        .expect("Could not convert the response to text")
         .to_owned();
     debug!("raw resp: {}", raw_text);
     let apod: APOD = serde_json::from_str(raw_text.as_str())
@@ -37,8 +39,11 @@ fn download_apod() {
     }
 
     // attempt hq url first, fallback if necessary
-    let image_url = apod.hdurl.unwrap_or_else(|| apod.url);
+    let image_url = apod.hdurl.unwrap_or(apod.url);
 
+    // FIXME: probably shouldn't just alwyas default to png if we don't know the file type but I am poopoo at rust so doing this for now
+    let binding = image_url.clone();
+    let image_file_type = path::Path::new(&binding).extension().and_then(OsStr::to_str).unwrap_or("png");
     debug!("HQ image URL: {}", image_url);
 
     // Would like to avoid panicking here, instead log errors
@@ -49,7 +54,9 @@ fn download_apod() {
             Ok(bytes) => {
                 image_byte_vec = bytes;
                 let image = image::load_from_memory(&image_byte_vec).unwrap();
-                image.save("test.png").unwrap();
+
+                // can probably do this nicer with a string interpolation or format
+                image.save(file_store_dir + &apod.date + "-" + &apod.title + "." + image_file_type).unwrap();
             }
             Err(byte_err) => error!("Failed to create image byte vector with error {}", byte_err),
         },
