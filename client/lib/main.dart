6b7b1ed3ad3bbf2034b3faca5b3d38c8671cc573
Garrett <gmoseke@protonmail.com>
2024-04-28 16:31:46 +0200

    feat: working backlog fetch from server + render

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,20 +1,118 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-void main() {
-  runApp(const MainApp());
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class _MyAppState extends State<MyApp> {
+  late Future<List<BacklogItem>> futureItems;
+
+  @override
+  void initState() {
+    super.initState();
+    futureItems = getBacklogItems();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      title: 'Khares',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
       home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Khares'),
+        ),
         body: Center(
-          child: Text('Hello World!'),
+          child: FutureBuilder<List<BacklogItem>>(
+            future: futureItems,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Text(snapshot.data!
+                    .map((backlogItem) =>
+                        "${backlogItem.category.name.toUpperCase()}: ${backlogItem.title} | ${backlogItem.progress.name} | ${backlogItem.rating ?? "unrated"} ")
+                    .join("\n"));
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              }
+
+              // By default, show a loading spinner.
+              return const CircularProgressIndicator();
+            },
+          ),
         ),
       ),
     );
   }
+}
+
+Future<List<BacklogItem>> getBacklogItems() async {
+  final resp = await http.get(Uri.parse("http://localhost:8000/backlog"));
+  if (resp.statusCode == 200) {
+    List<dynamic> itemsList = jsonDecode(resp.body);
+    return itemsList.map((item) => BacklogItem.fromJson(item)).toList();
+  }
+  throw Exception("Failed to load backlog items");
+}
+
+enum BacklogItemCategory { game, movie, book, show }
+
+enum BacklogItemProgress {
+  backlog,
+  inProgress,
+  complete,
+  dnf,
+}
+
+class BacklogItem {
+  final BacklogItemCategory category;
+  final String title;
+  final BacklogItemProgress progress;
+  final bool? favorite;
+  final bool? replay;
+  final String? notes;
+  final int? rating;
+  final String? genre;
+  // TODO: enable
+  // final List<String>? tags
+
+  BacklogItem(
+      {required this.category,
+      required this.title,
+      required this.progress,
+      this.favorite,
+      this.replay,
+      this.notes,
+      this.rating,
+      this.genre});
+
+  BacklogItem.fromJson(Map<String, dynamic> json)
+      : category = BacklogItemCategory.values.byName((json['category'] as String).toLowerCase()),
+        title = json['title'] as String,
+        progress = BacklogItemProgress.values.byName((json['progress'] as String).toLowerCase()),
+        favorite = json['favorite'] as bool?,
+        replay = json['replay'] as bool?,
+        notes = json['notes'] as String?,
+        rating = json['rating'] as int?,
+        genre = json['genre'] as String?;
+
+  Map<String, dynamic> toJson() => {
+        'category': category.toString(),
+        'title': title,
+        'progress': progress.toString(),
+        if (favorite != null) 'favorite': favorite,
+        if (replay != null) 'replay': replay,
+        if (notes != null) 'notes': notes,
+        if (rating != null) 'rating': rating,
+        if (genre != null) 'genre': genre
+      };
 }
