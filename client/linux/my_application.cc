342f0975ef214fa0a024f5b68affcd42cc3ff614
Garrett <gmosedev@proton.me>
2024-09-02 17:55:36 +0200

    feat: add app icon asset

--- client/linux/my_application.cc
+++ client/linux/my_application.cc
@@ -47,10 +47,15 @@ static void my_application_activate(GApplication* application) {
     gtk_window_set_title(window, "khares_client");
   }
 
-  gtk_window_set_default_size(window, 1280, 720);
+  gtk_window_set_default_size(window, 1920, 1080);
   gtk_widget_show(GTK_WIDGET(window));
 
   g_autoptr(FlDartProject) project = fl_dart_project_new();
+  g_autoptr(GError) error = nullptr;
+  gtk_window_set_icon_from_file(window, g_strconcat(fl_dart_project_get_assets_path(project), "/assets/libero.png", NULL), &error);
+  if (error != nullptr) {
+     g_warning("Failed to set icon: %s", error->message);
+  }
   fl_dart_project_set_dart_entrypoint_arguments(project, self->dart_entrypoint_arguments);
 
   FlView* view = fl_view_new(project);
